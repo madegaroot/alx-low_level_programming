baf98916f43d8f169b76bc30ec33ce315a070ed0
mpho <giftmadega@gmail.com>
2023-02-17 17:44:01 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints the alphabet in lowecase using putchar function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++);
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
+
